60d94a17b6f27de5a4ab6392949d57dfd8993c70
UltraScienceGuy <60634457+UltraScienceGuy@users.noreply.github.com>
2020-06-19 23:59:44 -0500

    Create frameList.py

---
+++ frameList.py
@@ -0,0 +1,6 @@
+import os
+
+list = os.listdir()
+print(list)
+
+#run this code, and then highlight all of the elements that have frame(integer). I pasted that into the list you see in frameCompare.
