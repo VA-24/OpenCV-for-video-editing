cec0b7e2c9021c303a48b49bae7f23f23674b33f
UltraScienceGuy <60634457+UltraScienceGuy@users.noreply.github.com>
2020-06-20 19:29:28 -0500

    Added listing capabilities

--- getFrame.py
+++ getFrame.py
@@ -2,11 +2,15 @@ import cv2
 
 #will need to run this first in order to generate the appr 393 jpg files that will be compared to a base image
 vidcap = cv2.VideoCapture('Test.mp4')
+frame_list = []
 
 success,image = vidcap.read()
 count = 0
 while success:
   cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  frame_list.append("frame%d.jpg" % count)
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
+print(frame_list)
+#copy the output this gives into frameCompare.py, naming the list frame_list
